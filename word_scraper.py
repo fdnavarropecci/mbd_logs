aade89c505ed24bbfcf22bc5aefc6048bea8dbd6
fdnavarropecci <fdnavarropecci@gmail.com>
2021-02-04 18:54:42 +0100

    save changes

---
+++ word_scraper.py
@@ -0,0 +1,16 @@
+    """
+
+    Author: Felipe David Navarro
+    Date: 04/02/2021
+    This Script scrapes the most common words in Spanish for the txt file downloaded from:
+    http://corpus.rae.es/frec/10000_formas.TXT
+    
+    """
+
+# Imports
+
+import pandas as pd
+import requests
+
+# 
+#palabras = 
